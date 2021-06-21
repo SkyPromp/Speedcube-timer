3c8d2f2c6c8d71caf526285862bdb1e947023328
Max Poppe <max.poppe@ugent.be>
2021-06-21 04:21:46 +0200

    resets time and runs time when the spacebar is released, problem: the time runs too slow compared to reality

--- start.js
+++ start.js
@@ -1,7 +1,8 @@
 window.addEventListener("keyup", checkKeyPress, false); // Listens for key releases and calls checkKeyPress function
 
 function checkKeyPress(key){
-    if (key.keyCode === 32){
-        alert("spatie is ingedrukt")
+    if (key.keyCode === 32){ // Check if the key is the spacebar
+        let timer = new StopWatch();
+        window.setInterval(timer.stopwatch, 1); // Call stopwatch every millisecond
     }
 }
--- timer.js
+++ timer.js
@@ -1,36 +1,42 @@
-let milliseconds = 0;
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
-
-function stopwatch(){
-    // calculate next time
-    milliseconds++;
-    if (milliseconds === 1000){
-        milliseconds = 0;
-        seconds++;
-        if (seconds === 60){
-            seconds = 0;
-            minutes++;
-            if (minutes === 60){
-                minutes = 0;
-                hours++;
-            }
-        }
+class StopWatch{
+    constructor() {
+        self.milliseconds = 0;
+        self.seconds = 0;
+        self.minutes = 0;
+        self.hours = 0;
     }
 
-    // Get leading zero's function
-    const zeroPad = (num, places) => String(num).padStart(places, '0');
+    stopwatch(){
+        // calculate next time
+        self.milliseconds++;
+        if (self.milliseconds === 1000){
+            self.milliseconds = 0;
+            self.seconds++;
+            if (self.seconds === 60){
+                self.seconds = 0;
+                self.minutes++;
+                if (self.minutes === 60){
+                    self.minutes = 0;
+                    self.hours++;
+                }
+            }
+        }
 
-    // Display time
-    if (hours !== 0){
-        document.getElementById("display").innerHTML = `${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${seconds}.${zeroPad(milliseconds, 3)}`;
-    }else if (minutes !== 0){
-        document.getElementById("display").innerHTML = `${zeroPad(minutes, 2)}:${seconds}.${zeroPad(milliseconds, 3)}`;
-    } else {
-        document.getElementById("display").innerHTML = `${seconds}.${zeroPad(milliseconds, 3)}`;
+        // Get leading zero's function
+        const zeroPad = (num, places) => String(num).padStart(places, '0');
+
+        // Display time
+        if (self.hours !== 0){
+            document.getElementById("display").innerHTML = `${zeroPad(self.hours, 2)}:${zeroPad(self.minutes, 2)}:${self.seconds}.${zeroPad(self.milliseconds, 3)}`;
+        }else if (self.minutes !== 0){
+            document.getElementById("display").innerHTML = `${zeroPad(self.minutes, 2)}:${self.seconds}.${zeroPad(self.milliseconds, 3)}`;
+        } else {
+            document.getElementById("display").innerHTML = `${self.seconds}.${zeroPad(self.milliseconds, 3)}`;
+        }
     }
 }
 
 
-window.setInterval(stopwatch, 1); // Call stopwatch every millisecond
+
+
+
