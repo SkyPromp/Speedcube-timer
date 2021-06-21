1e5fc46120b871a053f709a94d3f56611da36c93
Max Poppe <max.poppe@ugent.be>
2021-06-22 01:13:05 +0200

    changed text file to csv

--- cgi-bin/filewriter.py
+++ cgi-bin/filewriter.py
@@ -3,21 +3,21 @@ import datetime
 
 # Append the current date/time, scramble and solving time to the text file
 def write(scramble, time):
-    file = open("times.txt", "a")
+    file = open("times.csv", "a")
     file.write(f"{datetime.datetime.now()},{scramble},{time}\n")
     file.close()
 
 
 # Removes the last used line of the file (so removes the empty line, and the used line and then adds another newline)
 def remove():
-    file = open("times.txt", "r")
+    file = open("times.csv", "r")
     r = file.read()
     file.close()
     m = r.split("\n")
     s = "\n".join(m[:-2]) + "\n"  # remove last 2 lines (last line is \n)
     if len(s) == 1:
         s = ""
-    file = open("times.txt", "w+")
+    file = open("times.csv", "w+")
     for i in range(len(s)):
         file.write(s[i])
     file.close()
