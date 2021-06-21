61ec84f1caa2419c4ed2473fa47fc9fcf31d5ec8
Max Poppe <max.poppe@ugent.be>
2021-06-21 04:59:27 +0200

    timer now works in real time, the interval of the timer is now variable (4ms seems to be the sweetspot since at 2ms the timer isn't in real time anymore), also the timer now stops when the spacebar is pressed (and held down), TODO: keep the timer from resetting when releasing the spacebar after pausing the timer

--- start.js
+++ start.js
@@ -1,8 +1,20 @@
-window.addEventListener("keyup", checkKeyPress, false); // Listens for key releases and calls checkKeyPress function
+window.addEventListener("keyup", checkKeyUp, false); // Listens for key releases and calls checkKeyUp function
 
-function checkKeyPress(key){
+function checkKeyUp(key){
     if (key.keyCode === 32){ // Check if the key is the spacebar
-        let timer = new StopWatch();
-        window.setInterval(timer.stopwatch, 1); // Call stopwatch every millisecond
+        milliseconds = 0;
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        interval = window.setInterval(stopwatch, rate); // Call stopwatch every millisecond
     }
 }
+
+window.addEventListener("keydown", checkKeyDown, false); // Listens for key releases and calls checkKeyDown function
+
+function checkKeyDown(key){
+    if (key.keyCode === 32){ // Check if the key is the spacebar
+        window.clearInterval(interval);
+    }
+}
+
--- timer.js
+++ timer.js
@@ -1,42 +1,41 @@
-class StopWatch{
-    constructor() {
-        self.milliseconds = 0;
-        self.seconds = 0;
-        self.minutes = 0;
-        self.hours = 0;
-    }
+// Define millisecond interval rate (needs to be above 0 and a natural power of a divisor of 1000)
+let rate = 4;
 
-    stopwatch(){
-        // calculate next time
-        self.milliseconds++;
-        if (self.milliseconds === 1000){
-            self.milliseconds = 0;
-            self.seconds++;
-            if (self.seconds === 60){
-                self.seconds = 0;
-                self.minutes++;
-                if (self.minutes === 60){
-                    self.minutes = 0;
-                    self.hours++;
-                }
+// Define variables to hold time values
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+// Define variable to hold setInterval() function
+let interval = null;
+
+function stopwatch(){
+    // calculate next time
+    milliseconds += rate;
+    if (milliseconds === 1000){
+        milliseconds = 0;
+        seconds++;
+        if (seconds === 60){
+            seconds = 0;
+            minutes++;
+            if (minutes === 60){
+                minutes = 0;
+                hours++;
             }
         }
+    }
 
-        // Get leading zero's function
-        const zeroPad = (num, places) => String(num).padStart(places, '0');
+    // Get leading zero's function
+    const zeroPad = (num, places) => String(num).padStart(places, '0');
 
-        // Display time
-        if (self.hours !== 0){
-            document.getElementById("display").innerHTML = `${zeroPad(self.hours, 2)}:${zeroPad(self.minutes, 2)}:${self.seconds}.${zeroPad(self.milliseconds, 3)}`;
-        }else if (self.minutes !== 0){
-            document.getElementById("display").innerHTML = `${zeroPad(self.minutes, 2)}:${self.seconds}.${zeroPad(self.milliseconds, 3)}`;
-        } else {
-            document.getElementById("display").innerHTML = `${self.seconds}.${zeroPad(self.milliseconds, 3)}`;
-        }
+    // Display time
+    if (hours !== 0){
+        document.getElementById("display").innerHTML = `${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${seconds}.${zeroPad(milliseconds, 3)}`;
+    }else if (minutes !== 0){
+        document.getElementById("display").innerHTML = `${zeroPad(minutes, 2)}:${seconds}.${zeroPad(milliseconds, 3)}`;
+    } else {
+        document.getElementById("display").innerHTML = `${seconds}.${zeroPad(milliseconds, 3)}`;
     }
 }
-
-
-
-
 
