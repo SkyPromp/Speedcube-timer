9ec2ec7edd3093fa1b1b8fe86771ad3bd9b50ab2
Max Poppe <max.poppe@ugent.be>
2021-06-22 04:00:18 +0200

    added different backgrounds accessible in the settings menu and loop through them, also added different text colors to the settings menu and loop through them

---
+++ backgroundswitch.js
@@ -0,0 +1,12 @@
+let backgroundIndex = 0;
+let colorIndex = 0;
+
+function nextBackground() {
+    let backgrounds = ["gplaypattern", "triangular", "carbon_fibre", "cloudy-day", "connectwork", "dark-triangles", "footer_lodyas"];
+    document.querySelector("body").style.backgroundImage = `url("img/backgrounds/${backgrounds[++backgroundIndex % backgrounds.length]}.png")`;
+}
+
+function nextColor() {
+    let colors = ["black", "white"];
+    document.querySelector("body").style.color = colors[++colorIndex % colors.length];
+}
